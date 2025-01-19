a092a58900120c10a319b3f9426b29d43ec69958
Shivam31260 <shivamdeshmukh63@gmail.com>
2025-01-19 19:36:47 +0530

    h

--- firedetection.py
+++ firedetection.py
@@ -4,12 +4,13 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_FOLDER = os.path.join(BASE_DIR, 'static')
 MODEL_PATH = os.path.join(STATIC_FOLDER, 'firedetection.pt')
+ogmodel=os.path.join(STATIC_FOLDER, 'yolov5')
 
 def fire_detection_model(conf_threshold=0.6, iou_threshold=0.45):
     """
     Loads the YOLOv5 model with the specified settings.
     """
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=MODEL_PATH, force_reload=True)
+    model = torch.hub.load(ogmodel, 'custom', path=MODEL_PATH, force_reload=True,source='local')
     model.eval()
     model.conf = conf_threshold  # confidence threshold
     model.iou = iou_threshold    # IoU threshold
