bc1bdb07dcd85b5bc4bb0991f53c8fcfafc39291
Shivam31260 <shivamdeshmukh63@gmail.com>
2025-01-20 14:41:32 +0530

    h

--- firedetection.py
+++ firedetection.py
@@ -1,17 +1,31 @@
 import torch
 import os
+
 # Path to YOLOv5 model weights
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_FOLDER = os.path.join(BASE_DIR, 'static')
 MODEL_PATH = os.path.join(STATIC_FOLDER, 'firedetection.pt')
-ogmodel=os.path.join(STATIC_FOLDER, 'yolov5')
+ogmodel = os.path.join(STATIC_FOLDER, 'yolov5')
 
-def fire_detection_model(conf_threshold=0.6, iou_threshold=0.45):
+# Global variable to cache the loaded model
+_cached_model = None
+
+def facemask_model(conf_threshold=0.6, iou_threshold=0.45):
     """
-    Loads the YOLOv5 model with the specified settings.
+    Returns the cached YOLOv5 model if already loaded, or loads it for the first time.
     """
-    model = torch.hub.load(ogmodel, 'custom', path=MODEL_PATH, force_reload=True,source='local')
-    model.eval()
-    model.conf = conf_threshold  # confidence threshold
-    model.iou = iou_threshold    # IoU threshold
-    return model
+    global _cached_model
+    
+    # If the model is not cached yet, load it from the .pt file
+    if _cached_model is None:
+        _cached_model = torch.hub.load(ogmodel, 'custom', path=MODEL_PATH, force_reload=False, source='local')
+        _cached_model.eval()  # Set the model to evaluation mode
+        print("Model loaded for the first time.")
+    else:
+        print("Using cached model.")
+    
+    # Set thresholds for confidence and IoU
+    _cached_model.conf = conf_threshold
+    _cached_model.iou = iou_threshold
+    
+    return _cached_model
